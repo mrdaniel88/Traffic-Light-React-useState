e2f02b7e42ae2b3eda0cbd56d646684d2ac88032
Daniel Martinez <d.martinez74@gmail.com>
2023-04-04 02:35:19 +0000

    Traffic light using React and useState

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [color, setColor] = useState("")
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+
+			<div className="semaforo">
+				<div className="luces">
+					<div onClick={()=>setColor("red")} className={`${color == "red" ? "glow" : ""} red`}>						
+					</div>
+					<div onClick={()=>setColor("yellow")} className={`${color == "yellow" ? "glow" : ""} yellow`}>
+					</div>
+					<div onClick={()=>setColor("green")} className={`${color == "green" ? "glow" : ""} green`}>
+					</div>
+				</div>
+			</div>
+
 	);
 };
 
